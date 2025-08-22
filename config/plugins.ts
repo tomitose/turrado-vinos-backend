2fde08f4b4bead7922dbfba46b16300481f9c202
Tomas Tosello <tomitosello@gmail.com>
2025-08-22 16:19:00 -0300

    Primer commit del backend con Strapi

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,9 @@
-export default () => ({});
+// ./config/plugins.js
+module.exports = {
+    graphql: {
+      enabled: true,
+      config: {
+        playgroundAlways: true, // Mantiene el Playground activo en desarrollo
+      },
+    },
+  };
---
+++ src/api/vino/routes/vino.ts
@@ -0,0 +1,7 @@
+/**
+ * vino router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::vino.vino');
